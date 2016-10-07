5d8eafc2a95dd57755b4840253baf8d61f9f4fcc
bunjee <bunjee@omega.gg>
2016-10-07 10:49:50 +0200

    WBackendVlc: Update 'glTexParameterf' to 'glTexParameteri'

--- src/SkMedia/src/media/WBackendVlc.cpp
+++ src/SkMedia/src/media/WBackendVlc.cpp
@@ -1165,8 +1165,8 @@ WBackendVlc::WBackendVlc() : WAbstractBackend(new WBackendVlcPrivate(this))
                                  d->textures[i].width, d->textures[i].height,
                                  0, PLAYER_FORMAT, PLAYER_DATA_TYPE, 0);
 
-                    glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-                    glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+                    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+                    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
 
                     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
                     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
