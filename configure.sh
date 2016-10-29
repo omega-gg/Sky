5219b1d57bd37a57e6f7630cea20a1063710668e
bunjee <bunjee@omega.gg>
2016-10-29 16:05:36 +0200

    Configure: Update linux (2)

--- configure.sh
+++ configure.sh
@@ -130,14 +130,14 @@ fi
 # VLC
 #--------------------------------------------------------------------------------------------------
 
+echo "COPYING VLC"
+
 if [ $2 = "win32" ]; then
 
-    echo "COPYING VLC"
-
     cp -r "$VLC"/sdk/include/vlc include
-
-    cp "$VLC"/sdk/lib/libvlc*.* lib
 fi
+
+cp "$VLC"/sdk/lib/libvlc* lib
 
 #--------------------------------------------------------------------------------------------------
 # libtorrent
