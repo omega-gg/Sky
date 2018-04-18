eb14eeabb03f5fe517620c5f0e56c696c2fccc0f
bunjee <bunjee@omega.gg>
2018-04-18 11:20:17 +0200

    Scripts: Update Qt 5 (18)

--- 3rdparty.sh
+++ 3rdparty.sh
@@ -51,7 +51,7 @@ X11_ubuntu="libx11-dev libxi-dev libxinerama-dev libxrandr-dev libxcursor-dev li
 
 Qt4_ubuntu="qt4-default libqtwebkit-dev openssl"
 
-Qt5_ubuntu="qtdeclarative5-private-dev qtscript5-dev libqt5xmlpatterns5-dev libqt5svg5-dev"\
+Qt5_ubuntu="qtdeclarative5-private-dev qtscript5-dev libqt5xmlpatterns5-dev libqt5svg5-dev "\
 "libqt5x11extras5-dev"
 
 VLC_ubuntu="libvlc-dev vlc"
