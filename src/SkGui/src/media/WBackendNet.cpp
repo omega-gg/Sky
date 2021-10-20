a484af3f66c8ba96fd9c4b3e400b8e87e20e0e5d
bunjee <bunjee@omega.gg>
2021-10-20 15:07:11 +0200

    Sky 1.8.0-0 (2)

--- src/SkGui/src/media/WBackendNet.cpp
+++ src/SkGui/src/media/WBackendNet.cpp
@@ -93,7 +93,7 @@ void WBackendNetQuery::init(Type type, const QString & url)
     cookies    = false;
     skipError  = false;
 
-    maxHost =  5;
+    maxHost =  3;
     delay   =  0;
     timeout = -1;
 }
