0d06626414aeb9acd0636faf38d1729a8c19c741
bunjee <bunjee@omega.gg>
2021-11-26 21:22:59 +0100

    build: NDK 21

--- build.sh
+++ build.sh
@@ -40,7 +40,7 @@ WindowsKit_version="10"
 SDK_version="30"
 SDK_version_minimum="21"
 
-NDK_version="23"
+NDK_version="21"
 
 #--------------------------------------------------------------------------------------------------
 # environment
