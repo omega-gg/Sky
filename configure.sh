9011d8729befcb891a55edcf2d71c0af6c15d887
bunjee <bunjee@omega.gg>
2022-03-02 18:43:08 +0100

    Porting to iOS (18)

--- configure.sh
+++ configure.sh
@@ -126,7 +126,12 @@ elif [ $qt = "qt5" ]; then
 
     qx="5"
 else
-    Qt="$external/Qt/$Qt6_version"
+    if [ $1 = "ios" ]; then
+
+        Qt="$external/Qt/$Qt6_version/ios"
+    else
+        Qt="$external/Qt/$Qt6_version"
+    fi
 
     QtX="Qt6"
 
