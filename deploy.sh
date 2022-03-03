5250337cdb005087c45e42f6416e3546ac0e3725
bunjee <bunjee@omega.gg>
2022-03-03 17:22:22 +0100

    Porting to iOS (27)

--- deploy.sh
+++ deploy.sh
@@ -135,7 +135,12 @@ elif [ $qt = "qt5" ]; then
 
     qx="5"
 else
-    Qt="$external/Qt/$Qt6_version"
+    if [ $1 = "iOS" ]; then
+
+        Qt="$external/Qt/$Qt6_version/ios"
+    else
+        Qt="$external/Qt/$Qt6_version"
+    fi
 
     QtX="Qt6"
 
