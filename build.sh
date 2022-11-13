6dcc0ba95aa5077d5d677ae9df66e453bc8de348
bunjee <bunjee@omega.gg>
2022-11-13 18:24:12 +0100

    android: Update to NDK 22

--- build.sh
+++ build.sh
@@ -42,7 +42,7 @@ WindowsKit_version="10"
 SDK_version="31"
 SDK_version_minimum="21"
 
-NDK_version="23"
+NDK_version="22"
 
 #--------------------------------------------------------------------------------------------------
 # environment
