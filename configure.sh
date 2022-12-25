5cf1dc62c71967d6fbc2e8cdd8e5aa4625f889b5
bunjee <bunjee@omega.gg>
2022-12-25 15:25:53 +0100

    linux: Update to VLC snap (3)

--- configure.sh
+++ configure.sh
@@ -316,6 +316,16 @@ elif [ $1 = "macOS" ]; then
 
     echo "COPYING VLC"
 
+    cp -r "$VLC"/include/vlc include
+
+    cp "$VLC"/libvlc.so.5     lib/libvlc.so
+    cp "$VLC"/libvlccore.so.9 lib/libvlccore.so
+
+elif [ $1 = "linux" ]; then
+
+    echo "COPYING VLC"
+
+    # NOTE: Sometimes we're using libvlc-dev from the system.
     if [ -d "$VLC"/include ]
 
         cp -r "$VLC"/include/vlc include
@@ -323,15 +333,6 @@ elif [ $1 = "macOS" ]; then
 
     cp "$VLC"/lib/libvlc.5.dylib     lib/libvlc.dylib
     cp "$VLC"/lib/libvlccore.9.dylib lib/libvlccore.dylib
-
-elif [ $1 = "macOS" ]; then
-
-    echo "COPYING VLC"
-
-    cp -r "$VLC"/include/vlc include
-
-    cp "$VLC"/libvlc.so.5     lib/libvlc.so
-    cp "$VLC"/libvlccore.so.9 lib/libvlccore.so
 
 elif [ $1 = "iOS" ]; then
 
