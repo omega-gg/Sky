7c1a61901f81bb4b2fc15f60ffb46cebc760867d
bunjee <bunjee@omega.gg>
2023-04-28 17:04:53 +0200

    WControllerPlaylist: Add the 'related' support (2)

--- src/SkGui/src/controllers/WControllerPlaylist.cpp
+++ src/SkGui/src/controllers/WControllerPlaylist.cpp
@@ -394,11 +394,6 @@ void WControllerPlaylistData::applyRelated(const QByteArray & array, const QStri
     }
 
     origin = reader.extractString("related");
-
-    if (origin.isEmpty() == false)
-    {
-        type = WControllerPlaylist::Redirect;
-    }
 }
 
 void WControllerPlaylistData::applyHtml(const QByteArray & array, const QString & url)
