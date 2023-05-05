66197193ce3103dab3b8639a8feb942076aa1266
bunjee <bunjee@omega.gg>
2023-05-05 17:30:03 +0200

    Sky 1.8.1-1

--- src/SkGui/src/media/WTabTrack_patch.cpp
+++ src/SkGui/src/media/WTabTrack_patch.cpp
@@ -35,7 +35,7 @@ void WTabTrack_patch(QString & data, const QString & api)
 {
     qWarning("WTabTrack_patch: Patching.");
 
-    if (Sk::versionIsLower(api, "1.8.1-0"))
+    if (Sk::versionIsLower(api, "1.8.1-1"))
     {
         int index = data.indexOf("<tabs>");
 
