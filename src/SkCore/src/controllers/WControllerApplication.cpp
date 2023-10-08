45fc69e9ca01bceaed87e75aa6fc0ca67bf7e8fd
bunjee <bunjee@omega.gg>
2023-10-08 21:45:55 +0200

    SkMedia: Add multi-track support (132)

--- src/SkCore/src/controllers/WControllerApplication.cpp
+++ src/SkCore/src/controllers/WControllerApplication.cpp
@@ -1679,7 +1679,7 @@ QByteArray WControllerApplication::generateHmacSha1(const QByteArray & bytes,
 
     if (index == -1)
     {
-        return message;
+        return QString();
     }
     else return message.mid(index + 1);
 }
