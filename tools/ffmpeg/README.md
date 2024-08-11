acd75389951f9f1df20a62d684f39865abe28599
Benjamin Arnaud <barnaud@slb.com>
2024-08-11 15:46:33 +0200

    tools: Create ffmpeg (20)

--- tools/ffmpeg/README.md
+++ tools/ffmpeg/README.md
@@ -7,9 +7,9 @@ Place your FFmpeg binaries into the bin folder.
 
 ## Tools
 
-- concat.sh: Concatenate two videos into one.
-- cut.sh: Cut the video a the provided timestamps.
-- reverse.sh: Reverse the video.
-- loop.sh: Generate a looped version of the video.
-- volume.sh: Adjust the video's volume.
-- topaz.sh: Upscale the video.
+- [concat.sh](contat.sh): Concatenate two videos into one.
+- [cut.sh](cut.sh): Cut the video a the provided timestamps.
+- [reverse.sh](reverse.sh): Reverse the video.
+- [loop.sh](loop.sh): Generate a looped version of the video.
+- [volume.sh](volume.sh): Adjust the video's volume.
+- [topaz.sh](topaz.sh): Upscale the video.
--- tools/ffmpeg/topaz.sh
+++ tools/ffmpeg/topaz.sh
@@ -38,7 +38,7 @@ export TVAI_MODEL_DATA_DIR="$model"
 
 # NOTE: This line comes from Topaz Video AI (right click > FFmpeg command).
 "$ffmpeg" -y "-hide_banner" "-i" "$1" "-sws_flags" "spline+accurate_rnd+full_chroma_int" \
-"-filter_complex" "tvai_up=model=$3:scale=0:w=3840:h=2160:preblur=0:noise=0:details=0:halo=0:blur=0:compression=0:estimate=8:blend=0.2:device=0:vram=1:instances=1,scale=w=3840:h=2160:flags=lanczos:threads=0" \
+"-filter_complex" "tvai_up=model=$3:scale=0:w=3840:h=2160:preblur=0:noise=0:details=0:halo=0:blur=0:compression=0:estimate=8:blend=0.2:device=0:vram=1:instances=1,scale=w=3840:h=2160:flags=lanczos:threads=0:force_original_aspect_ratio=increase,crop=3840:2160" \
 "-c:v" "h264_nvenc" "-profile:v" "high" "-pix_fmt" "yuv420p" "-g" "30" "-preset" "p7" \
 "-tune" "hq" "-rc" "constqp" "-qp" "18" "-rc-lookahead" "20" "-spatial_aq" "1" \
 "-aq-strength" "15" "-b:v" "0" "-an" "-map_metadata" "0" "-map_metadata:s:v" "0:s:v" \
@@ -46,9 +46,10 @@ export TVAI_MODEL_DATA_DIR="$model"
 "-metadata" "videoai=Enhanced using $3; mode: auto; revert compression at 0; recover details at 0; sharpen at 0; reduce noise at 0; dehalo at 0; anti-alias/deblur at 0; focus fix Off; and recover original detail at 20. Changed resolution to 3840x2160" \
 "temp.mp4"
 
-# NOTE: This line comes from Topaz Video AI logs (Help > logging > Open current log).
-"$ffmpeg" -y -hide_banner -nostdin -i "temp.mp4" -i "$1" -c:v copy -map 0:v -an -map_metadata 0 \
--movflags use_metadata_tags -fps_mode passthrough "$2"
+# NOTE: This line comes from Topaz Video AI logs (Help > logging > Open current log). We remove the
+#       -nostdin option because it seems to make the output size vary, could be unrelated though.
+"$ffmpeg" -y "-hide_banner" "-i" "temp.mp4" "-i" "$1" "-c:v" "copy" "-map" "0:v" "-an" \
+"-map_metadata" "0" "-movflags" "use_metadata_tags" "-fps_mode" "passthrough" "$2"
 
 #--------------------------------------------------------------------------------------------------
 # Clean
