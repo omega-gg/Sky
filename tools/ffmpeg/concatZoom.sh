6324751563cf222b30676031c870be34e07d014c
Benjamin Arnaud <bunjee@omega.gg>
2025-07-18 12:35:19 +0200

    ffmpeg: Create concatZoom (4)

--- tools/ffmpeg/concatZoom.sh
+++ tools/ffmpeg/concatZoom.sh
@@ -16,16 +16,6 @@ height="2160"
 # Functions
 #--------------------------------------------------------------------------------------------------
 
-getWidth()
-{
-    "$ffprobe" -v error -select_streams v:0 -show_entries stream=width -of csv=p=0 "$1"
-}
-
-getHeight()
-{
-    "$ffprobe" -v error -select_streams v:0 -show_entries stream=height -of csv=p=0 "$1"
-}
-
 getFps()
 {
     "$ffprobe" -v 0 -select_streams v:0 -show_entries stream=r_frame_rate -of csv=p=0 "$1" \
@@ -90,35 +80,10 @@ done
 
 rm -rf frames
 
-ratio_input=$(awk "BEGIN { printf \"%.6f\", $(getWidth "$2") / $(getHeight "$2") }")
+echo "file 'temp.mp4'" >  videos.txt
+echo "file '$2'"       >> videos.txt
 
-ratio_target=$(awk "BEGIN { printf \"%.6f\", 16 / 9 }")
-
-echo "file 'temp.mp4'" > videos.txt
-
-is_equal=$(awk "BEGIN { diff = ($ratio_input - $ratio_target);\
-print (diff < 0.01 && diff > -0.01) ? 1 : 0 }")
-
-if [ "$is_equal" -eq 1 ]; then
-
-    echo "file '$2'" >> videos.txt
-
-    "$ffmpeg" -y -f concat -safe 0 -i videos.txt $codec -c:a copy "$3"
-else
-    # NOTE: When the input ratio is wrong, we stretch the video to 16:9.
-
-    echo "concatZoom: Stretching the input to 16:9."
-
-    "$ffmpeg" -y -i "$2" -vf "scale=${width}:${height}:flags=lanczos,setdar=16/9" \
-              -codec:v libx264 -preset veryslow -qp 0 \
-              -c:a copy temp2.mp4
-
-    echo "file 'temp2.mp4'" >> videos.txt
-
-    "$ffmpeg" -y -f concat -safe 0 -i videos.txt $codec -c:a copy "$3"
-
-    rm temp2.mp4
-fi
+"$ffmpeg" -y -f concat -safe 0 -i videos.txt $codec -c:a copy "$3"
 
 #--------------------------------------------------------------------------------------------------
 # Clean
