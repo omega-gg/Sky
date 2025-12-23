953bb272b2bc52a289d28b6ac83968382a0dc352
bunjee <bunjee@omega.gg>
2025-12-23 16:18:39 +0100

    Update VLC (10)

--- 3rdparty.sh
+++ 3rdparty.sh
@@ -14,8 +14,6 @@ compiler_win="mingw"
 
 qt="qt6"
 
-vlc="vlc3"
-
 mobile="simulator"
 
 #--------------------------------------------------------------------------------------------------
@@ -55,7 +53,6 @@ if [ "$2" = "all" ]; then
 
     sh environment.sh $compiler_win
     sh environment.sh $qt
-    sh environment.sh $vlc
     sh environment.sh $mobile
 fi
 
